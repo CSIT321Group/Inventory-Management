f8672ef10f8f17dac70b661ccd5759d3feb4e31a
AdrianORourke <105805725+AdrianORourke@users.noreply.github.com>
2023-07-28 01:19:19 +1000

    git commit

--- frontend/src/pages/inventory.js
+++ frontend/src/pages/inventory.js
@@ -28,30 +28,32 @@ const Inventory = () => {
             <button className='applyFilterButton'>APPLY FILTERS</button>
             <h1>Inventory Table</h1>
             <div className='component'>
-                <table className='inventoryTable'>
-                    <tr>
-                        <th>SKU</th>
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Supplier</th>
-                        <th>Location</th>
-                        <th>Last Update</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Total Value</th>
-                    </tr>
-                    <tr>
-                        <td>FHJH47U</td>
-                        <td>Water Tap</td>
-                        <td>Taps</td>
-                        <td>Blues Inc</td>
-                        <td>A5 - 78U</td>
-                        <td>10am 25/05/2023</td>
-                        <td>24</td>
-                        <td>$65</td>
-                        <td>$1560</td>
-                    </tr>
-                </table>
+                <div>
+                    <table className='inventoryTable'>
+                        <tr>
+                            <th>SKU</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Supplier</th>
+                            <th>Location</th>
+                            <th>Last Update</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Total Value</th>
+                        </tr>
+                        <tr>
+                            <td>FHJH47U</td>
+                            <td>Water Tap</td>
+                            <td>Taps</td>
+                            <td>Blues Inc</td>
+                            <td>A5 - 78U</td>
+                            <td>10am 25/05/2023</td>
+                            <td>24</td>
+                            <td>$65</td>
+                            <td>$1560</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </>
     );
