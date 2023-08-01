b50fd95ed24dff6e775f8bb805778add53f9ae9c
Brendan Alderton <meangraph@gmail.com>
2023-08-01 14:05:14 +1000

    Fix filter on full/partial first and last name

--- backend/src/main/java/CSIT321/CN03/Repository/StaffMemberRepository.java
+++ backend/src/main/java/CSIT321/CN03/Repository/StaffMemberRepository.java
@@ -20,6 +20,12 @@ public interface StaffMemberRepository extends JpaRepository<StaffMember, Long> 
     @Query("SELECT s FROM StaffMember s JOIN FETCH s.roles r JOIN FETCH r.permissions WHERE s.userName = ?1")
     StaffMember findByUserNameImpl(String username);
 
-    @Query("SELECT s FROM StaffMember s WHERE LOWER(s.userName) LIKE LOWER(CONCAT('%', ?1, '%')) OR LOWER(s.first_name) LIKE LOWER(CONCAT('%', ?1, '%')) OR LOWER(s.last_name) LIKE LOWER(CONCAT('%', ?1, '%')) OR CAST(s.id AS string) LIKE ?1")
+    @Query("SELECT s FROM StaffMember s WHERE " +
+            "LOWER(s.userName) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "CAST(s.id AS string) LIKE ?1 OR " +
+            "LOWER(CONCAT(s.first_name, ' ', s.last_name)) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(s.first_name) LIKE LOWER(CONCAT('%', ?1, '%')) OR " +
+            "LOWER(s.last_name) LIKE LOWER(CONCAT('%', ?1, '%'))")
     List<StaffMember> search(String searchText);
+
 }
