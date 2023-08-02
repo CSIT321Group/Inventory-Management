78de8996f480f399170e53fb9e1a760afd030133
AdrianORourke <105805725+AdrianORourke@users.noreply.github.com>
2023-08-02 12:30:31 +1000

    commit git

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react';import "./dashboard.scss";
+import React from 'react';
+import "./dashboard.scss";
 import Widget from '../components/widget/widget';
 import Chart from '../components/chart/Chart';
 import Featured from '../components/featured/Featured';
@@ -8,22 +9,27 @@ import Table from '../components/table/Table';
 const Dashboard = () => {
     return (
         <>
-        <div className="headings"><h1> QuickView </h1></div>
-          <div className="widgets">
-            <div className="bestSeller"> <Widget type="BestSeller"/></div>
-            <div className="lowStock"><Widget type="lowstock"/></div>
-            <div className="inStock"><Widget type="instock"/></div>
-            <div className="outOrder"><Widget type="out"/></div>
-          </div>
-          <div className="headings"><h1> Sales<t>&nbsp;</t>Insight </h1></div>
-          <div className="charts">
-            <Featured/>
-            <Chart/>
-          </div>
-          <div className="listContainer">
-            <div className="listTilte">Latest transactions</div>
-            <Table/>
-          </div>
+			<div className='header'>
+				<h1>Quick View</h1>
+			</div>
+			<div className='content'>
+
+			</div>
+			<div className="widgets">
+				<div className="bestSeller"> <Widget type="BestSeller"/></div>
+				<div className="lowStock"><Widget type="lowstock"/></div>
+				<div className="inStock"><Widget type="instock"/></div>
+				<div className="outOrder"><Widget type="out"/></div>
+			</div>
+			<div className="headings"><h1> Sales<t>&nbsp;</t>Insight </h1></div>
+			<div className="charts">
+				<Featured/>
+				<Chart/>
+			</div>
+			<div className="listContainer">
+				<div className="listTilte">Latest transactions</div>
+				<Table/>
+			</div>
        </>
        
     );
