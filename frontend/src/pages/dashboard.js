2f45140efc762892c847b009fce99be74fe3c3cd
parikchitB <116056901+parikchitB@users.noreply.github.com>
2023-08-02 12:09:54 +1000

    its the dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import "./dashboard.scss";
+import Widget from '../components/widget/widget';
+import Chart from '../components/chart/Chart';
+import Featured from '../components/featured/Featured';
+import Table from '../components/table/Table';
+
  
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Welcome to the Home page</h1>
-        </div>
+        <>
+        <div className="headings"><h1> QuickView </h1></div>
+          <div className="widgets">
+            <div className="bestSeller"> <Widget type="BestSeller"/></div>
+            <div className="lowStock"><Widget type="lowstock"/></div>
+            <div className="inStock"><Widget type="instock"/></div>
+            <div className="outOrder"><Widget type="out"/></div>
+          </div>
+          <div className="headings"><h1> Sales<t>&nbsp;</t>Insight </h1></div>
+          <div className="charts">
+            <Featured/>
+            <Chart/>
+          </div>
+          <div className="listContainer">
+            <div className="listTilte">Latest transactions</div>
+            <Table/>
+          </div>
+       </>
+       
     );
 };
  
 export default Dashboard;
+
+
+
