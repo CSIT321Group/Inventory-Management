896b3b9b71562c0d81f0aa26ee5c39ad32e10b53
parikchitB <116056901+parikchitB@users.noreply.github.com>
2023-10-22 20:38:25 +1100

    table fix

--- frontend/src/pages/table/Table.jsx
+++ frontend/src/pages/table/Table.jsx
@@ -16,7 +16,7 @@ const List = () => {
     const fetchData = async () => {
       const orderEndpoint = `http://localhost:8080/api/order`;
       const staffEndpoint = `http://localhost:8080/api/staff`;
-      const stockEndpoint= `http://localhost:8080/api/stock/random`;
+      const stockEndpoint = `http://localhost:8080/api/stock`;
 
       try {
         const [orderResponse, staffResponse, stockResponse] = await Promise.all([
@@ -37,31 +37,34 @@ const List = () => {
           }),
         ]);
 
-        // Create a mapping of staff data by their IDs for easy access
-        const staffDataMap = new Map();
-        staffResponse.data.forEach((staff) => {
-          staffDataMap.set(staff.id, staff);
-        });
-        const stockDataMap =new Map();
-        stockResponse.data.forEach((stock) => {
-          stockDataMap.set(stock.stockId, stock);
-        });
+        const orderData = orderResponse.data;
+        const staffData = staffResponse.data;
+        const stockData = stockResponse.data;
 
+        // Shuffle the stockData array to get a random order
+        const shuffledStockData = shuffleArray(stockData);
 
-        // Merge order and staff data with the same ID
-        const combinedData = orderResponse.data.map((order) => {
-          const staff = staffDataMap.get(order.id);
-          const stock = stockDataMap.get(order.stockId);
+        // Take the first 10 items from the shuffled list
+        const randomStockData = shuffledStockData.slice(0, 10);
+
+        const combinedData = randomStockData.map((stock, index) => {
+          const order = orderData[index];
+          const staff = staffData[index];
+
+          // Generate random timestamps
+          const orderDate = getRandomDate();
+          const deliveryDate = getRandomDate();
+
           return {
-            id: order.id,
-            product: stock ? stock.stock_name:"CNC-Extruder",
-            amount: stock ? stock.stock_quantity: 1,
-            first_name: staff ? staff.first_name : '',
-            orderDate: order.orderDate,
-            deliveryDate: order.deliveryDate,
+            id: index + 1,
+            product: stock.stock_name,
+            amount: stock.stock_quantity,
+            first_name: staff ? staff.first_name : 'N/A',
+            orderDate,
+            deliveryDate,
             status: order.status,
           };
-        }).slice(0,10);
+        });
 
         setData(combinedData);
       } catch (error) {
@@ -71,6 +74,23 @@ const List = () => {
 
     fetchData();
   }, []);
+
+  // Function to shuffle an array
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  // Function to generate a random date
+  const getRandomDate = () => {
+    const startDate = new Date(2022, 0, 1); // Start date as January 1, 2022
+    const endDate = new Date(); // Current date
+    const randomDate = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+    return randomDate.toISOString().slice(0, 10); // Format as YYYY-MM-DD
+  };
 
   return (
     <TableContainer component={Paper} className="table">
@@ -86,8 +106,8 @@ const List = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index}>
+          {data.map((row) => (
+            <TableRow key={row.id}>
               <TableCell className="tableCell">{row.id}</TableCell>
               <TableCell className="tableCell">
                 <div className="cellWrapper">{row.product}</div>
